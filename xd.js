9f996822f02bd81ef61f8b99629b78fa789bb437
LOOR-ZAMBRANO-ROBERTH-DAVID <56706913+LOOR-ZAMBRANO-ROBERTH-DAVID@users.noreply.github.com>
2021-05-22 15:02:05 -0500

    Tercer commmit

--- xd.js
+++ xd.js
@@ -2,3 +2,4 @@ var nombre='Jaime';
 function saludo(saludo){
     console.log('Hola mucho gusto como le va don ', nombre)
 }
+//Eran las 10 de la noche, patrullaba mi nave, era mi taxi un boxpaguen del anio 58
