7cae265a2942150224ba1492b6a15a51dddf47a2
LOOR-ZAMBRANO-ROBERTH-DAVID <dayvidlife@gmail.com>
2021-05-22 17:11:41 -0500

    Clase 1- Introduccion las funciones y creacion de variables

---
+++ Practicas-Clase/Semana1-Introduccion-Funciones/Introduccion-Funciones/funciones.js
@@ -0,0 +1,21 @@
+//Hay dos tipos de funciones, normales y de fecha
+
+function saludar(nombre){
+    console.log(`Hola como estas ${nombre}`);
+}
+
+const saludar2 = function(nombre)
+{
+    //this
+    console.log(`Hola como estas mi estimado ${nombre}`);
+}
+
+
+//Funciones arrow
+/**A las funciones normales tiene su propio contexto
+ * Una funcion flecha no lo necesita.
+ */
+
+const saludar3 = (nombre)=>{
+    
+}
