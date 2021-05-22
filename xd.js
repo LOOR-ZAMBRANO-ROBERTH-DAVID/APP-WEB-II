4d5c8da0353c92dac104921d6907fdc2fd529000
LOOR-ZAMBRANO-ROBERTH-DAVID <dayvidlife@gmail.com>
2021-05-22 15:41:30 -0500

    Cuarto commit xd

--- xd.js
+++
@@ -1,5 +0,0 @@
-var nombre='Jaime';
-function saludo(saludo){
-    console.log('Hola mucho gusto como le va don ', nombre)
-}
-//Eran las 10 de la noche, patrullaba mi nave, era mi taxi un boxpaguen del anio 58
