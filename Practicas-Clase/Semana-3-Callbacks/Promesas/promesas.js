394a1534b3546da9e28632987dbb8dc61e3dbbcb
LOOR-ZAMBRANO-ROBERTH-DAVID <dayvidlife@gmail.com>
2021-05-27 15:59:15 -0500

    Introduccion a Promesas

---
+++ Practicas-Clase/Semana-3-Callbacks/Promesas/promesas.js
@@ -0,0 +1,38 @@
+//Calbacks
+
+const libros =[
+    {
+        id: 1,
+        titulo: 'LIbro uno',
+        idautor:1
+    },
+    {
+        id: 2,
+        titulo: 'Libro dos',
+        idautor: 1   
+    },
+    {
+        id:3,
+        titulo: 'Libro 3',
+        idautor: 2
+    }
+]
+
+const autores =[
+    {
+        id:1,
+        nombre: 'Autor 1'
+    },
+    {
+        id:2,
+        nombre: 'Autor 2'
+    },
+    {
+        id: 3,
+        nombre: 'Autor 3'
+    }
+]
+
+function buscarLibroPorId(id){
+    
+}
