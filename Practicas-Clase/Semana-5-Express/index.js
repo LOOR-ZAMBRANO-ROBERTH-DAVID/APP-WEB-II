6bf2dc60151b94336d2c247628a0f117053e3a8e
LOOR-ZAMBRANO-ROBERTH-DAVID <dayvidlife@gmail.com>
2021-06-09 16:47:43 -0500

    Servicio Http sin el uso de Express

---
+++ Practicas-Clase/Semana-5-Express/index.js
@@ -0,0 +1,31 @@
+const fs =require("fs")
+const http =require("http");
+
+
+const home =fs.readFileSync("./index.html");
+const about =fs.readFileSync("./about.html");
+
+http.createServer((request, response)=>{
+    const {url} = request;
+    if(url==="/"){
+        response.writeHead(200, {"Content-Type":"text/html"});
+        response.write(home)
+        
+    }
+    else if(url==="/about"){
+        response.writeHead(200, {"Content-Type":"text/html"});
+        response.write(about)
+
+    }
+    else{
+        response.writeHead(404, {"Content-Type":"text/html"});
+        response.write("Ruta no existe uwu")
+    }
+    response.end();
+
+
+
+})
+.listen(8080,()=>{
+    console.log(`El servidor anda escuchando por el puerto 3000`);
+});
