ab8eb47b7f0b57b4708b92a9f93d8ebb810ccc4e
LOOR-ZAMBRANO-ROBERTH-DAVID <dayvidlife@gmail.com>
2021-06-10 10:13:07 -0500

    Trabajando en un ejemplo con express (get, post, delete)

--- Practicas-Clase/Semana-5-Express/index.js
+++ Practicas-Clase/Semana-5-Express/index.js
@@ -11,17 +11,67 @@ app.use(express.json());
 //get
 app.get('./', (req, res)=>{
     res.status(200).send(arreglo);
-})
+});
 
 
 //get?id
 
+app.get(':/nombre', (req, res)=>{
+    const {nombre} = req.params;
+    const respuesta = arreglo.filter(p=>{
+        return p.nombre===nombre
+    
+    })
+    if (respuesta.lenght>0){
+        res.status(200).send
+        respuesta[0]
+    }
+    
+});
+
+
 
 //post
+app.post('/', (req, res)=>{
+    const {body} = req;
+    arreglo.push(body);
+    res.send({
+        mensaje: "El elemento se almaceno correctamente",
+        respuesta:body
+    });
+})
 
-//path
+
+
+
+//put
+app.put('/', ()=>{
+    const {nombre, curso}=req;
+    let elemento = arreglo.filter(p=>p.nombre===nombre)[0];
+    elemento.curso=curso;
+    res.send({
+        respuesta:elemento
+    })
+
+});
+
+
 
 //delete
+app.delete('/', (req, res)=>{
+    const {nombre} = req.params;
+    arreglo=arreglo.filter(p=>p.nombre!==nombre);
+    res.status(200).send({
+        respuesta: "Eliminado correctamente"
+    })
+})
+
+
+
+
+
+
+
 
 
 app.listen(PORT, ()=>{
